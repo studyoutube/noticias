dec1a4cef612a256fb7c7afeacb005b851c004fe
studyoutube <repostudyoutube@outlook.com>
2022-06-28 13:14:03 -0300

    style formulario

--- search-news/src/app/components/navbar/navbar.component.ts
+++ search-news/src/app/components/navbar/navbar.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
+  styleUrls: ['./navbar.component.css'],
 })
 export class NavbarComponent implements OnInit {
+  @Input() titulo: string;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor() {
+    this.titulo = '';
   }
 
+  ngOnInit(): void {}
 }
